0d462b5745568b6d68495649414e0a7daf2c682d
mariapichardo <maria_pichardo@yahoo.com>
2020-04-08 16:45:08 -0400

    pass it to Saj

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,7 +1,43 @@
 'use strict';
 
 // Complete this algo
-const binarySearch = (array, target) => {
+const binarySearch = (array, target, midpointIndex = Math.floor(array.length / 2)) => {
+	let newMidPointIndex = midpointIndex ;
+
+	// [1,2,4,5,8] , 7
+
+	//[5,8]  7
+
+	// [8] index 4 
+
+
+	// with 3 
+
+	//[1, 2]  
+	
+	//[2]  midpoint =  1
+ 
+
+   //root node first 
+	if (array[midpointIndex] === target) {
+		return true
+	} 
+
+	if (midpointIndex < 2) {
+		return false
+	}
+
+	if (array[midpointIndex] > target) {
+		newMidPointIndex = Math.floor(newMidPointIndex / 2)
+	} else {
+		newMidPointIndex = Math.floor((array.length - (midpointIndex / 2)))
+	}
+
+
+
+
+
+
 	
 };
 
